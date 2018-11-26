ef2f1d2f7843bc0e6004a12f5f0fda4a2d278161
han <m15717671152@163.com>
2018-11-26 16:35:27 +0800

    shopping-car

--- application/views/home.php
+++ application/views/home.php
@@ -19,27 +19,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           <div class="dropdown col-sm-3 col-sm-offset-9 col-md-3 col-md-offset-9">
             <button class="btn-default">Shopping Cart</button>
             <div class="dropdown-content">
-              <p>
-                <a href="item.html">PS4</a>
-                <input></input>
-                <span class="price"> $249.99</span>
-              </p>
-              <p>
-                <a href="item.html">Bose QC30</a>
-                <input></input>
-                <span class="price"> $299.99</span>
-              </p>
-              <p>
-                <a href="item.html">Switch</a>
-                <input></input>
-                <span class="price"> $199.99</span>
-              </p>
-              <button class="btn-info checkout">Checkout</button>
+              <!-- 渲染 -->
+              <div class="shopping-car"> </div>
+              <!-- end -->
+              <button class="btn-info checkout col-sm-6">Checkout</button>
+              <p class="col-sm-2">total:$<span class="total-product">13123</span></p>
             </div>
           </div>
         </div>
       </div>
-      <div class="row container-fluid">
+      <div class="row container-fluid product-container">
         <div class="col-sm-3 col-md-2 sidebar">
           <ul class="nav nav-sidebar">
             <li class="active"><a href="#">All <span class="sr-only">(current)</span></a></li>
@@ -59,19 +48,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           <?php
             if($products){
               foreach($products as $row) {
+                $prod_id = $row->pid;
                 $prod_name = $row->name;
                 $prod_price = $row->price;
                 $prod_desc = $row->description;
                 echo
                 '<div class="col-lg-4 col-md-4 col-sm-6">
-                  <a href="item.php">
+                  <a href="item">
                     <img class="img-thumbnail" src="public/imgs/ps4-thumb.jpg" alt="PS4" width="240" height="240">
                     <p>' . $prod_name . '</p>
                   </a>
                   <div class="item-info">
                     <div class="item-price">$' . $prod_price . '</div>
                     <div class="item-add">
-                      <button class="btn-info">Add to Cart </button>
+                      <button class="btn-info add-product" data-id="' . $prod_id . '">Add to Cart </button>
                     </div>
                   </div>
                 </div>';
@@ -79,7 +69,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             }
           ?>
 
-          <button class="btn-info checkout" id="product_id_1">Get product detail via ajax</button>
+          <!-- <button class="btn-info checkout" id="product_id_1">Get product detail via ajax</button> -->
 
           </div>
         </div>
@@ -89,21 +79,113 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <script src="http://libs.baidu.com/jquery/2.1.1/jquery.min.js"></script>
     <script src="./public/js/bootstrap.min.js"></script>
     <script>
-            $("#product_id_1").click(function(){
-              //post the pictureID to controller and return the picture and embed it to the photoboard.
+
+      var home = {
+        init:function(){
+          this.addProduct();//点击添加购物车
+          this.getShoppingCarData();//获取购物车数据
+          this.changeProductNum();//购物车输入框改变
+        },
+        data:{
+          // productList:[]
+        },
+        addProduct:function(){
+          var that = this;
+          $('.product-container').on('click','.add-product',function(){
               $.ajax({
                   type:"post",
-                  data:{productid:1}, // data: "id=" + $(this).attr("id"),
+                  data:{productid:$(this).attr('data-id')},
                   url:"./ProductAPI/ProductDetail",
+                  dataType:'json',
                   success: function(data){
-                    console.log('ajax data', data);
-                    alert(data);
+                    that.setShoppingCar(data);
                   },
                   error: function() {
                     alert("ajax error");
                   }
-                  });
-            });
+              });
+          })
+        },
+
+        setShoppingCar:function(data){ //在 addProduct 中调用
+          var arr = JSON.parse(localStorage.getItem("shopCar")) || [];
+          var obj = {
+            name: data.name,
+            num: 1,
+            pid: data.pid,
+            price: data.price,
+          }
+          // 有重复的，num+1;否则扔进arr最后
+          function findProd(prod) { 
+              return prod.pid == data.pid;
+          }
+          if(arr.find(findProd)){
+            arr.find(findProd).num++;
+          }else {
+            arr.push(obj);
+          }
+          localStorage.setItem("shopCar",JSON.stringify(arr))
+          this.showShoppingCar(arr);
+        },
+
+        getShoppingCarData:function(){// 刷新页面 调用
+          var shopingList = JSON.parse(localStorage.getItem("shopCar")) || [];
+          this.showShoppingCar(shopingList);
+        },
+
+        showShoppingCar:function(data){// getShoppingCarData调用 + setShoppingCar调用
+          if(data.length){
+            $('.shopping-car').html('');
+          }else {
+            $('.shopping-car').html('nothing...');
+          }
+          
+          var total = 0;
+          data.forEach(function(item){
+            total += Number(item.price)*(item.num);
+            var shopHtml = `<p data-id="${item.pid}">
+                  <a href="item">${item.name}</a>
+                  <input style="min-width:50px" class="product-num" value="${item.num}" type="number" min="1" max="100"></input>
+                  <span class="price"> $${item.price}</span>
+                </p>`
+            $('.shopping-car').append($(shopHtml))
+          })
+          $('.total-product').html(total);
+        },
+
+        changeProductNum(){//改变写到locastorage中
+          $('.shopping-car').on('input','.product-num',function(){
+            // if($(this).val() > 100){
+
+            //   return
+            // }
+            var id = $(this).parent().attr('data-id');
+            var num = $(this).val() || 0;
+            var arr = JSON.parse(localStorage.getItem("shopCar")) || [];
+            function findProd(prod) { 
+              return prod.pid == id;
+            }
+            if(arr.find(findProd)){
+              arr.find(findProd).num = num;
+            }
+            localStorage.setItem("shopCar",JSON.stringify(arr))
+
+            //动态改价格
+            var total = 0;
+            arr.forEach(function(item){
+              total += Number(item.price)*(item.num);
+            })
+            $('.total-product').html(total);
+
+
+          })
+
+        }
+      }
+      
+
+      home.init();
+
           </script>
   </body>
 </html>
