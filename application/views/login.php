4c43ed7f3323b2d23fc15d6c23c8f6776c2238a2
han <m15717671152@163.com>
2018-11-29 15:50:44 +0800

    login

--- application/views/login.php
+++ application/views/login.php
@@ -1,21 +1,61 @@
 
 
 <div class="content-wrapper">
-    <?php
-        echo '<p>' . $err . '</p>';
-    ?>
     <div class="col-md-6">
         <h3> Login </h3>
-        <form id="login" method="post" action="login/login">
+        <form id="login">
             <div class="form-group">
                 <label>Email</label>
-                <input id="email" type="text" class="form-control" name="email"  required="true" >
+                <input id="email" type="text" class="form-control" name="email" >
             </div>
             <div class="form-group">
                 <label>Pwd</label>
-                <input id="pwd" type="password" class="form-control" name="pwd" required="true">
+                <input id="pwd" type="password" class="form-control" name="pwd">
             </div>
-            <button type="submit" class="btn btn-primary btn-product" value="Submit">Submit</button>
+            <button type="button" class="btn btn-primary" value="Submit">Submit</button>
         </form>
 	</div>
 </div>
+
+<script>
+    //  method="post" action="/login/login"
+    var logIn = {
+        init:function(){
+            this.login();
+        },
+        login:function(){
+            $('#login').on('click','.btn-primary',function(){
+                var formValue = $('#login').serializeArray();
+                var values = {};
+                for (x in formValue) {
+                    if (formValue[x].value) {
+                        values[formValue[x].name] = formValue[x].value;
+                    } else {
+                        alert(formValue[x].name + ' is required')
+                        return false;
+                    }
+                }
+                $.ajax({
+                    type: "post",
+                    data: values,
+                    url: "./Login/Login",
+                    dataType: 'json',
+                    success: function(data) {
+                        if(data.data.role == 0){
+                            window.location.href = window.location.href.split('login')[1]+'admin';
+                        } else if(data.data.role == 1) {
+                            window.location.href = window.location.href.split('login')[1]+'home';
+                        }else {
+                            alert('email or pwd error')
+                        }
+                    },
+                    error: function() {
+                        alert("ajax error");
+                    }
+                });
+            })
+        }
+    }
+    logIn.init();
+
+</script>
