375f768e5f8cfaf6d7b5f7dace5696e5ed5f9aa0
s4s <thea@163.com>
2018-12-09 19:09:33 +0800

    删除无用文件

--- bowen/web-programming/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-  try {
-    $dbh = new PDO("sqlite:./data.sq3");
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  } catch(PDOEXCEPTION $e) {
-    echo $e->getMessage();
-  }
-  echo "connected to sqlite DB\r\n";
-?>
